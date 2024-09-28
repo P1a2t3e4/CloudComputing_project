488f746e093c285eef7a237a9b1238dd44bdd500
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2024-09-28 15:06:37 +0000

    Update register.php

--- register.php
+++ register.php
@@ -29,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">

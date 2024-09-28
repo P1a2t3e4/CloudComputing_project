9614a6f54935367e0af6597b5af237a4ca9a8855
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2024-09-28 15:07:09 +0000

    Update login.php

--- login.php
+++ login.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
